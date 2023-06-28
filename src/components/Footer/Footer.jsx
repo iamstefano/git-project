0a01bca2eaa1806f2234d6afb2e8cc51ed22ff23
Iamstefanoe <stefanoar3@gmail.com>
2023-06-28 15:57:18 +0200

    feat: Footer & fixed App.css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer__content">Copyright 2023 || Edgemony ✨</p>
+    </div>
+  );
+};
+
+export default Footer;
